a5b529f0506bc4748790743032fcfd59cc7de069
Daniel Hobley <opdude@gmail.com>
2014-09-13 14:28:22 +0200

    Removed paint3d loading as it stopped the model tab loading

--- Truemax/moduleModel.py
+++ Truemax/moduleModel.py
@@ -1,15 +1,11 @@
 __author__ = 'sofiaelm'
 import manager
 import maya.cmds as cmds
-from pymel.all import mel
-from external import spPaint3dGui
 
 class ModuleModel(manager.Module):
 
     def __init__(self, mngr):
-        manager.Module.__init__(mngr)
-
-        self.spPaint3dwin = spPaint3dGui.spPaint3dWin()
+        manager.Module.__init__(self, mngr)
 
     def create_ui(self):
         tab = str(cmds.columnLayout())
