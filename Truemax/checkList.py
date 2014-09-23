fd43d23e0e555d9dcd739fb63924ebe1e6dae631
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-23 16:04:39 +0200

    updated checklist script with new scripts and fixed errors

--- Truemax/checkList.py
+++ Truemax/checkList.py
@@ -1,15 +1,13 @@
-
 from Truemax.checkNaming import name_compare
 from Truemax.checkTransforms import check_transforms
 from Truemax.detectHistory import detect_history
 from hfFixShading import hfCheckShading
-import hfFixShading as hfFS
-import maya.cmds as cmds
 
 
 def checkList():
-    if hfFS.hfCheckShading():
-        print "face assignment detected"
     if not check_transforms() or hfCheckShading() or not detect_history() or not name_compare():
+        print "found problems!"
+        return False
     else:
-        print "No face assignment"
+        print "No problems!"
+        return True
