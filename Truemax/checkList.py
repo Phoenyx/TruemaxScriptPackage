66a8eb7faf59674eb5e4c788ce720d11956ba13b
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-24 15:30:35 +0200

    checklist now prints to the command line

--- Truemax/checkList.py
+++ Truemax/checkList.py
@@ -1,15 +1,23 @@
+from Truemax import checkTransforms
 import Truemax.checkNaming as checkNaming
+import Truemax.detectHistory as detectHistory
 from Truemax.checkTransforms import check_transforms
+import Truemax.checkTransforms as check_transforms
 from Truemax.detectHistory import detect_history
 from hfFixShading import hfCheckShading
+import maya.cmds as cmds
+import sys
+
 
 reload(checkNaming)
+reload(checkTransforms)
+reload(detectHistory)
 
 
 def check_list():
-    if not check_transforms() or hfCheckShading() or not detect_history() or not checkNaming.name_compare():
-        print "found problems!"
+    if not checkTransforms.check_transforms() or hfCheckShading() or not detectHistory.detect_history() or not checkNaming.name_compare():
+        cmds.warning("Found problems! See Script Editor for more details")
         return False
     else:
-        print "No problems!"
+        sys.stdout.write("Object(s) checked. All is well, yay!\n")
         return True
