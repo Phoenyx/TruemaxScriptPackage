4adce89744beca4393b687b69f00d3b72c98b97f
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-25 13:40:01 +0200

    script now deletes groupId nodes created by fix face assignment script

--- Truemax/detectHistory.py
+++ Truemax/detectHistory.py
@@ -1,4 +1,5 @@
 from Truemax.checkNaming import get_top_node
+from hfFixShading import hfCheckShading
 
 __author__ = 'sofiaelm'
 
@@ -6,7 +7,18 @@ from pymel.all import *
 
 
 def detect_history():
+
     top_node = get_top_node()
+
+    #Delets groudIds created by hfFixShading script:
+    if hfCheckShading() is False:
+        unused_stuff = cmds.ls('*hyperLayout*', '*hyperView*', '*nodeEditorPanel*', '*nodeView*', '*groupId*')
+
+        if unused_stuff == []:
+            print "HAI"
+        else:
+            cmds.delete(unused_stuff)
+
 
     # Our top node is not correct and we can't check the history
     # until this is correct`
@@ -22,7 +34,7 @@ def detect_history():
     for obj in objects:
         history = cmds.listHistory(obj)
         if len(history) > 1:
-            cmds.warning(">> %s has construction history! <<" % obj)
+            cmds.warning(">> %s has construction history or face assignment! <<" % obj)
             return False
 
     return True
