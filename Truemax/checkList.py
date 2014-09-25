6f98d9506fde98a7a725ec09e0c5abdc52af8115
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-25 16:13:15 +0200

    Added script that checks for number of top nodes, added check pivot script

--- Truemax/checkList.py
+++ Truemax/checkList.py
@@ -1,19 +1,35 @@
 from Truemax import checkTransforms
 import Truemax.checkNaming as checkNaming
 import Truemax.detectHistory as detectHistory
+import Truemax.checkPivot as checkPivot
 from hfFixShading import hfCheckShading
 import sys
-
+import maya.cmds as cmds
 
 reload(checkNaming)
 reload(checkTransforms)
 reload(detectHistory)
 
+cmds.file(save=True, force=True)
+
+def top_node_count():
+    cmds.select(allDagObjects=1)
+    all_top_nodes = cmds.ls(sl=1)
+
+    if len(all_top_nodes) > 1:
+        cmds.warning("You have more than one top node!")
+        return False
 
 def check_list():
+    
+    # Saves the scene in case it crashes when running the check.
+    cmds.file(save=True, force=True)
+
     checklist = [
+        {"status": top_node_count(), "error": "More than one top node!"},
         {"status": checkNaming.name_compare(), "error": "Objects have incorrect naming"},
         {"status": checkTransforms.check_transforms(), "error": "Objects have non zero transforms!"},
+        {"status": checkPivot.check_pivot(), "error": "Pivot not at Origin!"},
         {"status": hfCheckShading() is False, "error": "Face assignment detected!"},
         {"status": detectHistory.detect_history(), "error": "Objects have construction history/face assignment"},
     ]
