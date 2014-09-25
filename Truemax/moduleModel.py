cb391cd7e433a9a6e754b40a1bd318aaa9f225e9
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-25 16:08:08 +0200

    Moved buttons + script to "Scene" tab

--- Truemax/moduleModel.py
+++ Truemax/moduleModel.py
@@ -16,10 +16,6 @@ class ModuleModel(manager.Module):
     def __init__(self, mngr):
         manager.Module.__init__(self, mngr)
 
-
-    def select_hierachy(self):
-        cmds.select(hi=1)
-
     def create_ui(self):
         tab = str(cmds.columnLayout())
         cmds.separator(style="none")
@@ -27,10 +23,6 @@ class ModuleModel(manager.Module):
         cmds.columnLayout()
         cmds.button(command=lambda *args: mel.FBImgpln(), label="Create Image Plane", backgroundColor=bg_colour)
         cmds.button(command=lambda *args: mel.FBpolyPlanarize(), label="Poly Planarize", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: self.select_hierachy(),  label="Select Hierachy", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: mel.FreezeTransformations(), label="Freeze Transformations", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: mel.DeleteHistory(), label="Delete History", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: mel.CenterPivot(), label="Center Pivot", backgroundColor=bg_colour)
         cmds.button(command=lambda *args: mel.ToggleFaceNormalDisplay(), label="Show Face Normals on Selected", backgroundColor=bg_colour)
         cmds.button(command=lambda *args: reverseNormals(), label="Reverse Normals on Selected", backgroundColor=bg_colour)
         cmds.setParent('..')
