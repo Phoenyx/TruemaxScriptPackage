59629b39e67f20d4c52b253802c9f46519608e09
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-29 09:35:44 +0200

    made a confirm dialog box for export script

--- Truemax/exportFBX.py
+++ Truemax/exportFBX.py
@@ -7,24 +7,29 @@ import os
 from pymel.all import *
 
 
+
 def export_asset():
+    continueDialog = cmds.confirmDialog(title='Make Reference',
+                                        message='Are you sure your model is ready to be exported?',
+                                        button=['Yes', 'No'], cancelButton='No', defaultButton='Yes', dismissString='No')
+    if continueDialog == "Yes":
+        scene_file_raw=str(cmds.file(q=1,sceneName=1,shortName=1))
+        scene_file=str(mel.match("[a*-z]+[A-Z]", scene_file_raw))
+        file_dir = os.path.dirname(cmds.file(q=1,sceneName=1))
+        export_dir = os.path.join(os.path.dirname(file_dir), "export")
 
-    scene_file_raw=str(cmds.file(q=1,sceneName=1,shortName=1))
-    scene_file=str(mel.match("[a*-z]+[A-Z]", scene_file_raw))
-    file_dir = os.path.dirname(cmds.file(q=1,sceneName=1))
-    export_dir = os.path.join(os.path.dirname(file_dir), "export")
+        if not os.path.exists(export_dir):
+            os.mkdir(export_dir)
 
-    if not os.path.exists(export_dir):
-        os.mkdir(export_dir)
-
-    scene_fbx = os.path.join(export_dir, scene_file + ".fbx")
-    assets = cmds.ls(sl=1)
-    len(assets)
-    if len(assets) == 0:
-        cmds.warning( ">>>>> Please select geometry <<<<<")
-    else:
-        # Not pretty but there is not exporting allowed in Python...
-        preset_file = "{0}{1}{2}".format(os.path.dirname(os.path.realpath(__file__)), os.path.sep,
-                                         "UnityExport.fbxexportpreset").replace("\\", "/")
-        mel.eval('FBXLoadExportPresetFile -f "{0}";'.format(preset_file))
-        mel.eval('FBXExport -f "{0}" -s;'.format(scene_fbx.replace("\\", "/")))
+        scene_fbx = os.path.join(export_dir, scene_file + ".fbx")
+        assets = cmds.ls(sl=1)
+        len(assets)
+        if len(assets) == 0:
+            cmds.warning( ">>>>> Please select geometry <<<<<")
+        else:
+            # Not pretty but there is not exporting allowed in Python...
+            preset_file = "{0}{1}{2}".format(os.path.dirname(os.path.realpath(__file__)), os.path.sep,
+                                             "UnityExport.fbxexportpreset").replace("\\", "/")
+            mel.eval('FBXLoadExportPresetFile -f "{0}";'.format(preset_file))
+            mel.eval('FBXExport -f "{0}" -s;'.format(scene_fbx.replace("\\", "/")))
+            cmds.warning( ">>>>> FBX Exported! <<<<<")
