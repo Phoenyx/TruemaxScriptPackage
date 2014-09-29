838d52882960212f13447c04235561b92a925d2a
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-09-29 09:34:43 +0200

    fixed PEP8 errors and changed colour for buttons if mj is user

--- Truemax/moduleModel.py
+++ Truemax/moduleModel.py
@@ -6,13 +6,13 @@ import maya.cmds as cmds
 from pymel.all import mel
 from reverseNormals import reverseNormals
 
-if get_author_initials()=='mj':
-    bg_colour=[0.9, 0.3, 0.5]
+if get_author_initials() == 'mj':
+    bg_colour = [0.9, 0.4, 1]
 else:
-    bg_colour=[0.4,0.4,0.4]
+    bg_colour = [0.4, 0.4, 0.4]
+
 
 class ModuleModel(manager.Module):
-
     def __init__(self, mngr):
         manager.Module.__init__(self, mngr)
 
@@ -23,13 +23,16 @@ class ModuleModel(manager.Module):
         cmds.columnLayout()
         cmds.button(command=lambda *args: mel.FBImgpln(), label="Create Image Plane", backgroundColor=bg_colour)
         cmds.button(command=lambda *args: mel.FBpolyPlanarize(), label="Poly Planarize", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: mel.ToggleFaceNormalDisplay(), label="Show Face Normals on Selected", backgroundColor=bg_colour)
-        cmds.button(command=lambda *args: reverseNormals(), label="Reverse Normals on Selected", backgroundColor=bg_colour)
+        cmds.button(command=lambda *args: mel.ToggleFaceNormalDisplay(), label="Show Face Normals on Selected",
+                    backgroundColor=bg_colour)
+        cmds.button(command=lambda *args: reverseNormals(), label="Reverse Normals on Selected",
+                    backgroundColor=bg_colour)
         cmds.setParent('..')
         cmds.setParent('..')
         cmds.setParent('..')
 
         return tab, "Model"
+
 
 def initModule(manager):
     return ModuleModel(manager)
