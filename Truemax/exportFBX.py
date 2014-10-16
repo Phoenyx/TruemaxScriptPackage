70e59e8091f159f9839965836623124b6d8aad63
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-10-16 12:52:37 +0200

    added an option for exporting animation or models as FBX

--- Truemax/exportFBX.py
+++ Truemax/exportFBX.py
@@ -1,35 +1,57 @@
 # Author: Sofia Elena Jakobsen Manioudakis
 # Version: 1.0
 # Export script for Unity Game models
-import maya.cmds as cmds
-import maya.mel as mel
-import os
-from pymel.all import *
 
+import sys
+from pymel.all import *
+from Truemax.checkNaming import get_top_node
 
 
 def export_asset():
-    continueDialog = cmds.confirmDialog(title='Make Reference',
+    continueDialog = cmds.confirmDialog(title='Make Model FBX',
                                         message='Are you sure your model is ready to be exported?',
-                                        button=['Yes', 'No'], cancelButton='No', defaultButton='Yes', dismissString='No')
-    if continueDialog == "Yes":
-        scene_file_raw=str(cmds.file(q=1,sceneName=1,shortName=1))
-        scene_file=str(mel.match("[a*-z]+[A-Z]", scene_file_raw))
-        file_dir = os.path.dirname(cmds.file(q=1,sceneName=1))
+                                        button=['Model', 'Animation', 'No'], cancelButton='No', defaultButton='No',
+                                        dismissString='No')
+    if continueDialog == "Model":
+        top_node = get_top_node()
+        file_dir = os.path.dirname(cmds.file(q=1, sceneName=1))
         export_dir = os.path.join(os.path.dirname(file_dir), "export")
 
         if not os.path.exists(export_dir):
             os.mkdir(export_dir)
 
-        scene_fbx = os.path.join(export_dir, scene_file + ".fbx")
-        assets = cmds.ls(sl=1)
-        len(assets)
-        if len(assets) == 0:
-            cmds.warning( ">>>>> Please select geometry <<<<<")
+        scene_fbx = os.path.join(export_dir, str(top_node) + ".fbx")
+
+        if cmds.objExists(str(top_node)) == 0:
+            cmds.warning(">>>>> Top node named incorrectly or non-existent <<<<<")
+
         else:
+            cmds.select(top_node)
             # Not pretty but there is not exporting allowed in Python...
             preset_file = "{0}{1}{2}".format(os.path.dirname(os.path.realpath(__file__)), os.path.sep,
                                              "UnityExport.fbxexportpreset").replace("\\", "/")
             mel.eval('FBXLoadExportPresetFile -f "{0}";'.format(preset_file))
             mel.eval('FBXExport -f "{0}" -s;'.format(scene_fbx.replace("\\", "/")))
-            cmds.warning( ">>>>> FBX Exported! <<<<<")
+            sys.stdout.write(">>>>> FBX Model Exported! <<<<<")
+
+    if continueDialog == "Animation":
+        top_node = get_top_node()
+        file_dir = os.path.dirname(cmds.file(q=1, sceneName=1))
+        export_dir = os.path.join(os.path.dirname(file_dir), "export")
+
+        if not os.path.exists(export_dir):
+            os.mkdir(export_dir)
+
+        scene_fbx = os.path.join(export_dir, str(top_node) + "ANIM" + ".fbx")
+
+        if cmds.objExists("geo_grp") == 0:
+            cmds.warning(">>>>> No group matches name 'geo_grp' <<<<<")
+
+        else:
+            cmds.select("geo_grp")
+            # Not pretty but there is not exporting allowed in Python...
+            preset_file = "{0}{1}{2}".format(os.path.dirname(os.path.realpath(__file__)), os.path.sep,
+                                             "UnityExport.fbxexportpreset").replace("\\", "/")
+            mel.eval('FBXLoadExportPresetFile -f "{0}";'.format(preset_file))
+            mel.eval('FBXExport -f "{0}" -s;'.format(scene_fbx.replace("\\", "/")))
+            sys.stdout.write(">>>>> FBX with Animation Exported! <<<<<")
