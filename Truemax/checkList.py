56a08a3e1d05ff4cef9a91d67ba96f7d39cd2351
Sofia Elena Jakobsen Manioudakis <sofiaelenajm@gmail.com>
2014-10-31 13:54:52 +0100

    Check list now checks for namespaces aswell

--- Truemax/checkList.py
+++ Truemax/checkList.py
@@ -9,6 +9,7 @@ import maya.cmds as cmds
 reload(checkNaming)
 reload(checkTransforms)
 reload(detectHistory)
+# reload(deleteDPLayers)
 
 def top_node_count():
     cmds.select(allDagObjects=1)
@@ -18,22 +19,34 @@ def top_node_count():
         cmds.warning("You have more than one top node!")
         return False
 
-def check_list():
 
+def namespaceCheck():
+    # Script that check for namespaces
+    # namespace "UI" and "shared" are builtin namespaces and cannot be deleted.
+    # List namespaces
+    namespaces = cmds.namespaceInfo(listOnlyNamespaces=True)
+
+    # If more namespaces return error
+    if (len(namespaces) > 2):
+        cmds.warning("Namespaces found!")
+        return False
+
+
+def check_list():
     checklist = [
-        {"status": top_node_count(), "error": "More than one top node!"},
+        {"status": top_node_count(), "error": "More than one top node"},
         {"status": checkNaming.name_compare(), "error": "Objects have incorrect naming"},
-        {"status": checkTransforms.check_transforms(), "error": "Objects have non zero transforms!"},
-        {"status": checkPivot.check_pivot(), "error": "Pivot not at Origin!"},
-        {"status": hfCheckShading() is False, "error": "Face assignment detected!"},
-        {"status": detectHistory.detect_history(), "error": "Objects have construction history/face assignment"},
+        {"status": checkTransforms.check_transforms(), "error": "Objects have non zero transforms"},
+        {"status": checkPivot.check_pivot(), "error": "Pivot not at Origin"},
+        {"status": hfCheckShading() is False, "error": "Face assignment detected"},
+        {"status": detectHistory.detect_history(), "error": "Objects have construction history"},
+        {"status": namespaceCheck(), "error": "Namespaces found"},
     ]
 
     errors = []
     for c in checklist:
         if c["status"] is False:
             errors.append(c["error"])
-
 
     if len(errors):
         return False, errors
